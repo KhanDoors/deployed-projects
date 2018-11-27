76965228ed0f5a069ec61741456ac13fb7893614
unknown <syyadkhan@yahoo.com>
2018-11-26 19:10:08 -0500

    nov261910

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const graphqlHTTP = require("express-graphql");
 
 const app = express();
 
+app.use("/graphql", graphqlHTTP({}));
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(4000, console.log(`Server started on port ${PORT}`));

788d61aba4aceb8d79551f0e3217f98b404817ab
unknown <syyadkhan@yahoo.com>
2018-11-27 19:55:33 -0500

    mongodb connected mongoose installed

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb://syyad:Cowboys1@ds031865.mlab.com:31865/deployedprojects"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to DB");
+});
 
 app.use(
   "/graphql",

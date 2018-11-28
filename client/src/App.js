66a9db80ce85c4662256fabc62b7528fe217c6f4
unknown <syyadkhan@yahoo.com>
2018-11-28 13:24:48 -0500

    cors installed queries linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import BookList from "./components/BookList";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Let's go Baby!!!</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Let's go Baby!!!</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
